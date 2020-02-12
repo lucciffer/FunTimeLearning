22917955820621b2c2d7fc52f4a8e0f5247947d8
Lucciffer <51166059+lucciffer@users.noreply.github.com>
2020-02-12 13:22:36 +0530

    Update hny.py

--- hny.py
+++ hny.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 msg = input("Enter the message string: ")
+#enter the message string and th eprogram shall print a pattern for the entered string
 
 ml = len(msg)
 for r in range(ml):
