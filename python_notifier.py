53c30fbcd9715fb5be2192bf557f36dbdd143855
Lucciffer <51166059+lucciffer@users.noreply.github.com>
2020-04-22 12:49:55 +0530

    Update python_notifier.py

--- python_notifier.py
+++ python_notifier.py
@@ -1,3 +1,6 @@
 #this program sends a desired notification to the system
 from plyer import notification
 notification.notify(title="shut the machine down and get ready", message="bye now!", timeout=10)
+
+
+#lucciffer
