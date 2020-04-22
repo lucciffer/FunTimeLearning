b5096f27dc4ef2918d9d69789b51d0095a9da804
Lucciffer <51166059+lucciffer@users.noreply.github.com>
2020-04-22 12:43:47 +0530

    Add files via upload

---
+++ coronaUpdates.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.worldometers.info/coronavirus/"
+r = requests.get(url)
+s = BeautifulSoup(r.text,"html.parser")
+data = s.find_all("div",class_ = "maincounter-number")
+
+print("Total cases: ",data[0].text.strip())
+print("Total deaths: ",data[1].text.strip())
+print("Total recovered: ",data[2].text.strip())
